da8c25df2022a94b303dab59aa61afdcbcdbc510
Yuan Liu <liuyuanneu@gmail.com>
2016-07-11 21:59:00 -0400

    Server Setup

--- server/server.js
+++ server/server.js
@@ -3,13 +3,23 @@ var requestHandler = require('./request-handler.js')
 var app = express();
 var port = process.env.PORT || 8000;
 
+var morgan = require('morgan');
+var parser = require('body-parser');
+
+//Logging and parsing
+
+app.use(morgan('dev'));
+app.use(parser.json());
+
+//Set up our routes
+app.get('/api/group',requestHandler.fetchGroup);
+app.post('/api/user',requestHandler.addUser);
+
+//Serve the client files
+app.use(express.static(__dirname + '/../client'));
 
 app.listen(port);
 
 console.log('server now listening in port' + port);
-
-//routing here
-app.get('/api/group',requestHandler.fetchGroup);
-app.post('/api/user',requestHandler.addUser);
 
 module.exports = app;
